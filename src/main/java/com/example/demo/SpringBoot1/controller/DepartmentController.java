180d40f95e3b1931631166940bf9bf48c51c45d8
Dev Singhal <singhaldev26@gmail.com>
2024-05-23 13:52:12 +0530

    init commit

--- src/main/java/com/example/demo/SpringBoot1/controller/DepartmentController.java
+++ src/main/java/com/example/demo/SpringBoot1/controller/DepartmentController.java
@@ -4,6 +4,8 @@ package com.example.demo.SpringBoot1.controller;
 import com.example.demo.SpringBoot1.entity.Department;
 import com.example.demo.SpringBoot1.service.ServiceLayer;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +16,24 @@ public class DepartmentController {
     @Autowired
     private ServiceLayer serviceLayer;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartments(@Valid @RequestBody Department department){
+        LOGGER.info("The save Departments method has been intitated");
         return serviceLayer.saveDepartments(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartment(){
+        LOGGER.info("The fetch list Departments method has been intitated");
         return serviceLayer.fetchDepartment();
     }
 
     @GetMapping("/departments/{id}")
     public Department fetchDepartmentById(@PathVariable("id") Long departmentID){
+        LOGGER.info("The fetch list Departments method has been intitated");
+
         return serviceLayer.fetchDepartmentbyId(departmentID);
     }
     @DeleteMapping("/departments")
--- src/main/java/com/example/demo/SpringBoot1/entity/Department.java
+++ src/main/java/com/example/demo/SpringBoot1/entity/Department.java
@@ -5,9 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -19,45 +27,4 @@ public class Department {
     public String departmentType;
     public Integer employees;
 
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentType() {
-        return departmentType;
-    }
-
-    public void setDepartmentType(String departmentType) {
-        this.departmentType = departmentType;
-    }
-
-    public Integer getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(Integer employees) {
-        this.employees = employees;
-    }
-
-    public Department(Long departmentId, String departmentName, String departmentType, Integer employees) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentType = departmentType;
-        this.employees = employees;
-    }
-
-    public Department() {
-    }
 }
