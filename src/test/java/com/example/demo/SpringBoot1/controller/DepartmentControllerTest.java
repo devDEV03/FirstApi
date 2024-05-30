5fc78c76c7e2f8479e247e9da9fe2c92bee8b5d7
Dev Singhal <singhaldev26@gmail.com>
2024-05-30 22:12:18 +0530

    init commit

--- src/test/java/com/example/demo/SpringBoot1/controller/DepartmentControllerTest.java
+++ src/test/java/com/example/demo/SpringBoot1/controller/DepartmentControllerTest.java
@@ -1,14 +1,27 @@
 package com.example.demo.SpringBoot1.controller;
 
+import com.example.demo.SpringBoot1.entity.Department;
+import com.example.demo.SpringBoot1.service.ServiceLayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@WebMvcTest(DepartmentController.class)
 class DepartmentControllerTest {
 
+
+    @MockBean
+    private ServiceLayer serverTest;
+
+
+    private Department dep;
     @BeforeEach
     void setUp() {
+      dep = Department.builder().departmentId(1L).departmentName("CSE").departmentType("Eng").employees(30).build();
     }
 
     @Test
